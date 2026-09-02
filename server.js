01b90ada0e0080a8e11f988242787b31523210ae
cfestoaires-glitch <cfestoaires@gmail.com>
2026-09-02 01:00:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const app = express();
-
+cons
 const PORT = process.env.PORT || 3000;
 
 // Tenta servir a pasta public
